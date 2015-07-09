1c344bcc0fd5ef36346e9ad0f4315266797d04f8
Corbin Otwell <cbotwell@gmail.com>
2015-07-09 13:52:56 -0500

    updated brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,6 @@
 var merge = require('broccoli-merge-trees');
 var compileSass = require('broccoli-sass');
 
-var appCss = compileSass(['styles', 'bower_components'], 'style.scss', 'style.css', 'fontawesome/css/font-awesome.css', 'reset-css/reset.css');
+var appCss = compileSass(['styles', 'bower_components'], 'style.scss', 'style.css');
 
 module.exports = merge(['public', 'bower_components', appCss]);
